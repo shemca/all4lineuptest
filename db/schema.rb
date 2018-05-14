8c2d812dbbf8f06633e92582f328abf20ef1770e
Shem <shemca@gmail.com>
2018-05-13 23:22:39 -0400

    third panel

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180513235828) do
+ActiveRecord::Schema.define(version: 20180514020826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20180513235828) do
     t.string "interior"
     t.string "vin"
     t.string "drivetrain"
+    t.string "transmission"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+    Car.create(name: '2017 MINI COOPER CLUBMAN ALL4', price: "$32,997.00", photo: "99b0164a-089e-400b-9b89-42718e30db4a.jpg", retailer: "MINI St. Catharines", kilometres: "13,095", transmission: "Automatic", exterior: "Pepper White", interior: "Carbon Black Leatherette", vin: "2F27071", drivetrain: "AWD")
+    Car.create(name: "2017 MINI COOPER COUNTRYMAN ALL4", price: "$40,187.25.00", photo: "4004618_259920343.jpg", retailer: "MINI Calgary", kilometres: "6,948", transmission: "Automatic", exterior: "Chesnut", interior: "Carbon Black Leatherette", vin: "2F27071", drivetrain: "AWD")
